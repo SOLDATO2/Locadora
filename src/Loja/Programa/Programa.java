ae12e89d63079f8ec95b65c03e0ddff530f70e0e
SOLDATO2 <felipeporto553@gmail.com>
2023-04-12 20:42:30 -0300

    modified:   src/Loja/Programa/Programa.java

--- src/Loja/Programa/Programa.java
+++ src/Loja/Programa/Programa.java
@@ -5,12 +5,21 @@ public class Programa {
     public static void main(String[] args){
         EstoqueLoja CDs = new EstoqueLoja();
         Filme CdTerrorGenerico = new Filme("Olhos Famintos", "2");
+        Filme CdAcaoGenerico = new Filme("Indiana Jones", "1");
 
         //adiciona objeto criado na lista terror da classe "CDs" de tipo Filme
         CDs.addTerror(CdTerrorGenerico);
+        CDs.addAcao(CdAcaoGenerico);
+
 
         System.out.println(CDs.getListaDeTerror().get(0).getNome());
         System.out.println(CDs.getListaDeTerror().get(0).getQnt());
+
+        System.out.println(CDs.getListaDeAcao().get(0).getNome());
+        System.out.println(CDs.getListaDeAcao().get(0).getQnt());
+
+
+
 
 
     }
