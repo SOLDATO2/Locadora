3fac21525158697893a0d9e8486f5c7b72585dc9
aluno <Aluno@PR01-COMP1-28.CSED.LAB>
2023-04-18 11:34:18 -0300

    implementacao inicial de clientes

--- src/Loja/View/Menu.java
+++ src/Loja/View/Menu.java
@@ -11,7 +11,7 @@ public class Menu {
         EstoqueLoja CDs = new EstoqueLoja();
         EstoqueLoja VHS = new EstoqueLoja();
         List<Funcionario> listaFuncionarios = new ArrayList<>(); // seria necessario exigir a criação de um funcionario antes usar o programa caso esse lista tivesse dentro da classe Funcionario.java
-        List<Cliente> listaClientes = new ArrayList<>();
+        List<Cliente> listaClientes = new ArrayList<>(); // substituir por classe Aluguel ( classe intermediaria )
         int escolha;
         String nome;
         int quantidade;
@@ -215,9 +215,108 @@ public class Menu {
                     }
                 }
                 // FAZER SISTEMA DE CLIENTES 17/04/23
-            }else if(escolha == 5){ // REGISTRO DE ALUGUEL
-                System.out.println("Ainda não foi feito.");
-            }else if(escolha == 6){// CONSULTA DE ALUGUEL
+            }else if(escolha == 5){ // REGISTRO DE CLIENTE ( EFETUAR ALUGUEL)
+                String nomeCliente, cpfCliente;
+                System.out.println("QUAL É O NOME DO CLIENTE?");
+                nomeCliente = scannerString.nextLine();
+                System.out.println("DIGITE O CPF DO CLIENTE:");
+                cpfCliente = scannerString.nextLine();
+
+                System.out.println("ESCOLHA UMA DAS OPCOES:");
+                System.out.println("[1] CDs");
+                System.out.println("[2] VHS");
+
+                escolha = scannerInteiro.nextInt();
+
+                if(escolha == 1){
+                    if(listasEstaoVaziasUnica(CDs) == true){
+                        System.out.println("Não é possivel alugar CDs pois o estoque está vazio.");
+                    }else{
+                        System.out.println("ESCOLHA UM GENERO:");
+                        if(!(CDs.getListaDeTerror().isEmpty())){ // mostra as categorias que não estão vazias
+                            System.out.println("[1] TERROR");
+                        }
+                        if(!(CDs.getListaDeAcao().isEmpty())){
+                            System.out.println("[2] ACAO");
+                        }
+                        if(!(CDs.getListaDeDrama().isEmpty())){
+                            System.out.println("[3] DRAMA");
+                        }
+                        if(!(CDs.getListaDeComedia().isEmpty())){
+                            System.out.println("[4] COMEDIA");
+                        }
+
+                        escolha = scannerInteiro.nextInt();
+                        int linha = 0;
+                        if(escolha == 1){
+                            List<Filme> filmesTerror = CDs.getListaDeTerror();
+                            for(linha = 0; linha < filmesTerror.size(); linha++){
+                                if(filmesTerror.get(linha).getQnt() > 0){
+                                    System.out.println("["+linha+"]"+":");
+                                    System.out.println("Nome: " + CDs.getListaDeTerror().get(linha).getNome());
+                                    System.out.println("Quantidade: "+ CDs.getListaDeTerror().get(linha).getNome());
+                                }
+                            }
+
+                            System.out.println("ESCOLHA UMA OPÇÃO (0, 1, 2, etc...):");
+                            escolha = scannerInteiro.nextInt();
+
+                            Filme filme = filmesTerror.get(escolha);
+
+                            int quantidadeFilme = scannerInteiro.nextInt();
+
+
+                            //fazer sistema para pedir quantidade de filmes que o cliente quer alugar. Tambem fazer um if para verificar se a quantidade que o usuario quer
+                            // é <= a quantidade de estoque do filme, e por fim subtrair o estoque - quantidadeFilme
+
+
+
+                            
+                            //Cliente cliente = new Cliente(nomeCliente, cpfCliente, filme.getNome(), quantidade)
+
+                            
+
+
+                            
+                        }else if(escolha == 2){
+                            List<Filme> filmesAcao = CDs.getListaDeAcao();
+                            for(linha = 0; linha < CDs.getListaDeAcao().size(); linha++){
+                                if(CDs.getListaDeAcao().get(linha).getQnt() > 0){
+                                    System.out.println("["+linha+"]"+":");
+                                    System.out.println("Nome: " + CDs.getListaDeAcao().get(linha).getNome());
+                                    System.out.println("Quantidade: "+ CDs.getListaDeAcao().get(linha).getNome());
+                                }
+                            }
+                        }else if(escolha == 3){
+                            List<Filme> filmesDrama = CDs.getListaDeDrama();
+                            for(linha = 0; linha < CDs.getListaDeDrama().size(); linha++){
+                                if(CDs.getListaDeDrama().get(linha).getQnt() > 0){
+                                    System.out.println("["+linha+"]"+":");
+                                    System.out.println("Nome: " + CDs.getListaDeDrama().get(linha).getNome());
+                                    System.out.println("Quantidade: "+ CDs.getListaDeDrama().get(linha).getNome());
+                                }
+                            }
+                        }else if(escolha == 4){
+                            List<Filme> filmesComedia = CDs.getListaDeComedia();
+                            for(linha = 0; linha < CDs.getListaDeComedia().size(); linha++){
+                                if(CDs.getListaDeComedia().get(linha).getQnt() > 0){
+                                    System.out.println("["+linha+"]"+":");
+                                    System.out.println("Nome: " + CDs.getListaDeComedia().get(linha).getNome());
+                                    System.out.println("Quantidade: "+ CDs.getListaDeComedia().get(linha).getNome());
+                                }
+                            }
+                        }
+
+                        
+                    }
+                }else if(escolha == 2){
+                    if(listasEstaoVaziasUnica(VHS) == true){
+                        System.out.println("Não é possivel alugar VHS pois o estoque está vazio.");
+                    }else{
+                        System.out.println("Ainda nao foi feito");
+                    }
+                }
+            }else if(escolha == 6){// CONSULTA DE ALUGUEIS
                 if(listasEstaoVazias(CDs, VHS) == true){
                     System.out.println("Não é possivel registrar um aluguel pois não existe nenhum filme em estoque.");
                 }else if(listaClientes.isEmpty()){
@@ -300,5 +399,17 @@ public class Menu {
         }
         
     }
+
+    private Boolean listasEstaoVaziasUnica(EstoqueLoja x){
+        if((x.getListaDeTerror().isEmpty()) && (x.getListaDeAcao().isEmpty()) && (x.getListaDeDrama().isEmpty()) && (x.getListaDeComedia().isEmpty())){
+            return true;
+        }else{
+            return false;
+        }
+    }
     
 }
+
+
+
+
